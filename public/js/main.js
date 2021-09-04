f7093af3f53c423775ae14cdcb1eae3891c34bb3
Ercan Ünal <ercanunal@anayurtteknoloji.com>
2021-09-04 20:07:02 +0300

    frontend connection

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,39 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
 const socket = io();
 
+//Message from server
 socket.on("message", (message) => {
   console.log(message);
+  outputMessage(message);
+
+  //Scroll down on a new message
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+// Message submit
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Get message text
+  const msg = e.target.elements.msg.value;
+  // Emit message to server
+  socket.emit("chatMessage", msg);
+
+  // Clear input and focus after submit
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta"> ${message.username} <span> ${message.time}</span></p>
+    <p class="text">
+    ${message.text}
+    </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { format } = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,16 +13,41 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Run when client connects
-
-io.on("connection", (socket) => {
-  console.log("New WS Connection..");
-
-  socket.emit("message", "Welcome to chat erchat!");
-});
-
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () =>
   console.log(`🚀🚀🚀 Server running on port ${PORT} 🚀🚀🚀`)
 );
+
+const helper = "erchat bot";
+
+// Run when client connects
+
+io.on("connection", (socket) => {
+  console.log("New WS Connection..");
+
+  //only the connected user (current user) will see the message
+
+  socket.emit("message", formatMessage(helper, "Welcome to erchat!"));
+
+  // Broadcast when a user connects
+  //emits everybody exept the user
+  socket.broadcast.emit(
+    "message",
+    formatMessage(helper, "A user has joined the chat")
+  );
+
+  //emits everbody
+  io.emit();
+
+  //RUns when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(helper, "A user has left the chat"));
+  });
+
+  //Listen for chatMessage
+
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessage("USER", msg));
+  });
+});
