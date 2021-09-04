0c6a303aacd1930629b8b7c1b464be100ead2ff0
Ercan Ünal <ercanunal@anayurtteknoloji.com>
2021-09-04 18:49:24 +0300

    socket connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//Set static folder
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Run when client connects
+
+io.on("connection", (socket) => {
+  console.log("New WS Connection..");
+
+  socket.emit("message", "Welcome to chat erchat!");
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () =>
+  console.log(`🚀🚀🚀 Server running on port ${PORT} 🚀🚀🚀`)
+);
